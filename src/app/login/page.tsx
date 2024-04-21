e2c78452d7dfea73f903b2980d11d3dac2b11c32
fabrice madre <stepwiseltd@gmail.com>
2024-04-21 22:00:41 +0400

    login and signup behaviours (WIP)

---
+++ src/app/login/page.tsx
@@ -0,0 +1,97 @@
+'use client'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
+const Login = () => {
+  const [email, setEmail] = useState<String>('');
+  const [password, setPassword] = useState('');
+  const [passwordConfirmation, setPasswordConfirmation] = useState('');
+  const [submit, setSubmit] = useState('Sign Up');
+
+  const [isSignup, setIsSignup] = useState(false);
+
+  useEffect(() => {
+    if (isSignup) {
+      setSubmit('Login');
+    } else {
+      setSubmit('Sign Up');
+    }
+  }, [isSignup]);
+
+  const handleLogin = async (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+    const loginUrl = 'http://localhost:3000/login';
+    const signupUrl = 'http://localhost:3000/signup';
+    const loginData = {
+      "user": {
+        "email": email,
+        "password": password,
+      }
+    }
+
+    const signupData = {
+      "user": {
+        "email": email,
+        "password": password,
+        "password_confirmation": passwordConfirmation,
+      }
+    }
+
+    const endpoint = isSignup ? { url: signupUrl, data: signupData } : { url: loginUrl, data: loginData };
+
+    try {
+      const response = await axios.post(endpoint.url, endpoint.data);
+      const { token } = response.data;
+      // Assuming you have a function to save the token to localStorage
+      localStorage.setItem('token', token);
+      // Redirect the user to dashboard or any other page
+      alert('Login successful');
+      window.location.href = '/home';
+    } catch (error) {
+      console.error('Login failed:', error);
+      // Handle login failure (display error message, etc.)
+    }
+  };
+
+  return (
+    <div className="bg-yellow-400 dark:bg-gray-800 h-screen overflow-hidden flex items-center justify-center min-h-screen">
+      <div className="bg-white lg:w-6/12 md:7/12 w-8/12 shadow-3xl rounded-xl mt-32">
+        <div className="bg-gray-800 shadow shadow-gray-200 absolute left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-full p-4 md:p-8">
+          <svg width="32" height="32" viewBox="0 0 24 24" fill="#FFF">
+            <path d="M0 3v18h24v-18h-24zm6.623 7.929l-4.623 5.712v-9.458l4.623 3.746zm-4.141-5.929h19.035l-9.517 7.713-9.518-7.713zm5.694 7.188l3.824 3.099 3.83-3.104 5.612 6.817h-18.779l5.513-6.812zm9.208-1.264l4.616-3.741v9.348l-4.616-5.607z" />
+          </svg>
+        </div>
+        <form className="p-12 md:p-24" onSubmit={handleLogin}>
+          <h2 className="text-2xl md:text-4xl text-black font-bold mb-6 md:mb-12">{isSignup ? "Sign Up" : "Login"}</h2>
+          <div className="flex items-center text-lg mb-6 md:mb-8">
+            <svg className="absolute ml-3" width="24" viewBox="0 0 24 24">
+              <path d="M20.822 18.096c-3.439-.794-6.64-1.49-5.09-4.418 4.72-8.912 1.251-13.678-3.732-13.678-5.082 0-8.464 4.949-3.732 13.678 1.597 2.945-1.725 3.641-5.09 4.418-3.073.71-3.188 2.236-3.178 4.904l.004 1h23.99l.004-.969c.012-2.688-.092-4.222-3.176-4.935z" />
+            </svg>
+            <input type="email" id="email" className="bg-gray-200 rounded pl-12 py-2 md:py-4 focus:outline-none w-full" placeholder="email" onChange={(e) => setEmail(e.target.value)} />
+          </div>
+          <div className="flex items-center text-lg mb-6 md:mb-8">
+            <svg className="absolute ml-3" viewBox="0 0 24 24" width="24">
+              <path d="m18.75 9h-.75v-3c0-3.309-2.691-6-6-6s-6 2.691-6 6v3h-.75c-1.24 0-2.25 1.009-2.25 2.25v10.5c0 1.241 1.01 2.25 2.25 2.25h13.5c1.24 0 2.25-1.009 2.25-2.25v-10.5c0-1.241-1.01-2.25-2.25-2.25zm-10.75-3c0-2.206 1.794-4 4-4s4 1.794 4 4v3h-8zm5 10.722v2.278c0 .552-.447 1-1 1s-1-.448-1-1v-2.278c-.595-.347-1-.985-1-1.722 0-1.103.897-2 2-2s2 .897 2 2c0 .737-.405 1.375-1 1.722z" />
+            </svg>
+            <input type="password" id="password" className="bg-gray-200 rounded pl-12 py-2 md:py-4 focus:outline-none w-full" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+          </div>
+          {isSignup && (
+            <div className="flex items-center text-lg mb-6 md:mb-8">
+              <svg className="absolute ml-3" viewBox="0 0 24 24" width="24">
+                <path d="M20.822 18.096c-3.439-.794-6.64-1.49-5.09-4.418 4.72-8.912 1.251-13.678-3.732-13.678-5.082 0-8.464 4.949-3.732 13.678 1.597 2.945-1.725 3.641-5.09 4.418-3.073.71-3.188 2.236-3.178 4.904l.004 1h23.99l.004-.969c.012-2.688-.092-4.222-3.176-4.935z" />
+              </svg>
+              <input type="password" id="passwordConfirmation" className="bg-gray-200 rounded pl-12 py-2 md:py-4 focus:outline-none w-full" placeholder="Confirm Password" onChange={(e) => setPasswordConfirmation(e.target.value)} />
+            </div>
+          )}
+          <div className="flex flex-col items-center justify-between mb-6 md:mb-8">
+            <button type="submit" className="bg-gradient-to-b from-gray-700 to-gray-900 font-medium p-2 md:p-4 text-white uppercase w-full rounded">Submit</button>
+            <button type="button" className="text-sm text-gray-600" onClick={() => setIsSignup(!isSignup)}>{submit}</button>
+            <button type="button" className="text-sm text-gray-600">Forgot Password?</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default Login
---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,33 @@
+import Image from 'next/image'
+import logo from '../../../public/next.svg'
+
+const NavBar = () => {
+  return (
+    <header
+      className="fixed inset-x-0 top-0 z-30 mx-auto w-full max-w-screen-md border border-gray-100 bg-white/60 py-3 shadow backdrop-blur-lg md:top-6 md:rounded-3xl lg:max-w-screen-lg">
+      <div className="px-4">
+        <div className="flex items-center justify-between">
+          <div className="flex shrink-0">
+            <a aria-current="page" className="flex items-center" href="/">
+              <Image className="h-7 w-auto" src={logo} alt="" />
+                <p className="sr-only">Bon Zanana Limited</p>
+            </a>
+          </div>
+          <div className="hidden md:flex md:items-center md:justify-center md:gap-5">
+            <a aria-current="page"
+              className="inline-block rounded-lg px-2 py-1 text-sm font-medium text-gray-900 transition-all duration-200 hover:bg-gray-100 hover:text-gray-900"
+              href="#">Our Application</a>
+            <a className="inline-block rounded-lg px-2 py-1 text-sm font-medium text-gray-900 transition-all duration-200 hover:bg-gray-100 hover:text-gray-900"
+              href="#">Pricing</a>
+          </div>
+          <div className="flex items-center justify-end gap-3">
+            <a className="inline-flex items-center justify-center rounded-xl bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm transition-all duration-150 hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+              href="/login">Login</a>
+          </div>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default NavBar
